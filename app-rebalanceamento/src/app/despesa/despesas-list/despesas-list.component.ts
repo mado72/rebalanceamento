8b7b8d59d56d7a9d6f883eda96b50f68c52bcf61
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-05-06 01:33:53 -0300

    Ajuste fino despesas list

--- app-rebalanceamento/src/app/despesa/despesas-list/despesas-list.component.ts
+++ app-rebalanceamento/src/app/despesa/despesas-list/despesas-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { DateTime } from 'luxon';
-import { DespesaRecorrenteImpl, Mes } from '../models/despesa.model';
+import { DespesaRecorrenteImpl, Mes, Periodicidade } from '../models/despesa.model';
 import { DespesasService } from '../services/despesas.service';
 import { toArray } from 'rxjs';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -15,7 +15,11 @@ type Pagamentos = {[classe: string] : {[mes: string] : DespesaRecorrenteImpl | u
 })
 export class DespesasListComponent implements OnInit {
 
+  private despesas: DespesaRecorrenteImpl[] = [];
+
   pagamentos : Pagamentos = {};
+
+  private _visao!: Periodicidade;
 
   private modalService = inject(NgbModal);
 
@@ -24,30 +28,67 @@ export class DespesasListComponent implements OnInit {
   ngOnInit(): void {
     this.obterDespesas();
   }
+
+  public get visao(): Periodicidade {
+    return this._visao;
+  }
+  public set visao(value: Periodicidade) {
+    this._visao = value;
+
+    this.despesas.forEach(despesa => {
+      const descricao = despesa.descricao || 'Não classificado';
+      let periodo;
+      switch (this.visao) {
+        case Periodicidade.SEMANAL:
+        case Periodicidade.QUINZENAL:
+        case Periodicidade.MENSAL:
+          periodo = DateTime.fromJSDate(despesa.dataVencimento).day.toString();
+          break;
+        case Periodicidade.TRIMESTRAL:
+        case Periodicidade.SEMESTRAL:
+        case Periodicidade.ANUAL:
+        default:
+          periodo = Object.keys(Mes)[DateTime.fromJSDate(despesa.dataVencimento).month-1];
+          break;
+      }
+      this.pagamentos[descricao] = this.pagamentos[descricao] || {};
+      this.pagamentos[descricao][periodo] = despesa;
+    });
+  }
+
+  get tiposPeriodo() {
+    return [Periodicidade.ANUAL, Periodicidade.SEMESTRAL, Periodicidade.TRIMESTRAL, Periodicidade.MENSAL, Periodicidade.QUINZENAL, Periodicidade.SEMANAL];
+  }
   
   private obterDespesas() {
     const ateData = DateTime.now().endOf("year").toJSDate();
     this.pagamentos = {};
 
     this.despesasService.obterDespesas().subscribe(despesas => {
-      despesas = despesas.flatMap(despesa => {
+      this.despesas = despesas.flatMap(despesa => {
         const projecao = despesa.programacaoDespesas(ateData);
         projecao.push(despesa);
         return projecao;
       });
-      despesas.forEach(despesa => {
-        const descricao = despesa.descricao || 'Não classificado';
-        const mesVencimento = despesa.mesVencimento;
-        if (!!mesVencimento) {
-          this.pagamentos[descricao] = this.pagamentos[descricao] || {};
-          this.pagamentos[descricao][mesVencimento] = despesa;
-        }
-      });
+      this.visao = Periodicidade.ANUAL;
     });
   }
 
-  get meses() {
-    return Object.keys(Mes);
+  get periodos() {
+    switch (this.visao) {
+      case Periodicidade.SEMANAL:
+      case Periodicidade.QUINZENAL:
+      case Periodicidade.MENSAL:
+        const mes = Object.keys(Periodicidade).indexOf(this.periodoCorrente);
+        const primeiroDia = DateTime.now().set({month: mes, day: 1});
+        const ultimoDia = primeiroDia.endOf('month');
+        return [...Array(ultimoDia.day)].map((_d,index)=>index+1).map(d=>d.toString() as string);
+      case Periodicidade.TRIMESTRAL:
+      case Periodicidade.SEMESTRAL:
+      case Periodicidade.ANUAL:
+        default:
+        return Object.keys(Mes);
+    }
   }
 
   get classesDespesas() {
@@ -62,21 +103,54 @@ export class DespesasListComponent implements OnInit {
     return (a, b) => !a ? (!b ? 0 : 1) : (!b ? -1 : a - b);
   }
 
-  get mesCorrente() {
-    const mesCorrente = DateTime.now().month - 1;
-    return Object.values(Mes)[mesCorrente];
+  get periodosCorrente() {
+    switch (this.visao) {
+      case Periodicidade.SEMANAL:
+      case Periodicidade.QUINZENAL:
+      case Periodicidade.MENSAL:
+        const mes = Object.keys(Periodicidade).indexOf(this.periodoCorrente);
+        const primeiroDia = DateTime.local(DateTime.now().year, mes, 1);
+        const ultimoDia = primeiroDia.endOf('month');
+        return [...Array(ultimoDia.day)].map(d=>d+1).map(d=>d.toString() as string);
+      case Periodicidade.TRIMESTRAL:
+      case Periodicidade.SEMESTRAL:
+      case Periodicidade.ANUAL:
+        default:
+        return Object.keys(Mes);
+    }
   }
-
+  
+  get periodoCorrente() {
+    switch (this.visao) {
+      case Periodicidade.SEMANAL:
+      case Periodicidade.QUINZENAL:
+      case Periodicidade.MENSAL:
+        return DateTime.now().day.toString();
+      case Periodicidade.TRIMESTRAL:
+      case Periodicidade.SEMESTRAL:
+      case Periodicidade.ANUAL:
+        default:
+          const mesCorrente = DateTime.now().month - 1;
+          return Object.values(Mes)[mesCorrente];
+    }
+  }
+  
   tipo(v: any) {
     return typeof v;
   }
-
-  anteriorMesCorrente(mes: any) {
-    const meses = this.meses;
-    const atual = this.mesCorrente || Mes.JANEIRO;
-    const idxMes = meses.indexOf(mes);
-    const idxAtual = meses.indexOf(atual);
-    return idxMes < idxAtual;
+  
+  anteriorMesCorrente(periodo: any) {
+    switch (this.visao) {
+      case Periodicidade.SEMANAL:
+      case Periodicidade.QUINZENAL:
+      case Periodicidade.MENSAL:
+        return parseInt(periodo) < DateTime.now().day;
+      case Periodicidade.TRIMESTRAL:
+      case Periodicidade.SEMESTRAL:
+      case Periodicidade.ANUAL:
+        default:
+          return parseInt(periodo) < DateTime.now().month - 1;
+    }
   }
   
   get totaisClasse() {
@@ -97,6 +171,10 @@ export class DespesasListComponent implements OnInit {
       });
     });
     return totais;
+  }
+
+  get totais() {
+    return Object.values(this.totaisMes).reduce((acc,vl)=>acc+=vl, 0);
   }
 
   abrirDespesaForm(despesa: DespesaRecorrenteImpl, titulo: string) {
