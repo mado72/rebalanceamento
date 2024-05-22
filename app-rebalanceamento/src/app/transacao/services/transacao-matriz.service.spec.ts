4d7494265151cf90eebd6cca5ecff6e3c0ec2af1
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-05-22 19:56:21 -0300

    transacao-matriz.service

--- app-rebalanceamento/src/app/transacao/services/transacao-matriz.service.spec.ts
+++ app-rebalanceamento/src/app/transacao/services/transacao-matriz.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed } from '@angular/core/testing';
 import { TransacaoMatrizService } from './transacao-matriz.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToastrModule } from 'ngx-toastr';
+import { ITransacao, Periodicidade, TipoLiquidacao, TipoTransacao, TransacaoImpl } from '../models/transacao.model';
 
 describe('TransacaoMatrizService', () => {
   let service: TransacaoMatrizService;
@@ -20,4 +21,187 @@ describe('TransacaoMatrizService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('montar matriz não deve sobrepor ocorrencias de uma mesma descricao ', () => {
+    const transacoes : TransacaoImpl[] = [
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        periodicidade: Periodicidade.MENSAL,
+        dataInicial: new Date(2020, 0, 1),
+        dataFinal: new Date(2020, 10, 1),
+        liquidacao: TipoLiquidacao.CONTA
+      },
+      {
+        _id: '123',
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        periodicidade: Periodicidade.MENSAL,
+        dataInicial: new Date(2020, 2, 1),
+        dataFinal: new Date(2020, 11, 1),
+        dataLiquidacao: new Date(2020, 2, 1),
+        liquidacao: TipoLiquidacao.CONTA
+      }    
+    ].map(i=>new TransacaoImpl(i));
+    
+    const matriz = service.montarMatriz(transacoes);
+    const t1 = matriz.get('Transacao 1');
+    expect(t1).toBeDefined();
+
+    if (!t1) throw "t1 não definido";
+
+    for (var mes = 0; mes < 12; mes++) {
+      expect(t1[mes]).toBeDefined();
+      expect(t1[mes].length * (mes+1)).toBe(mes+1);
+    }
+  })
+
+  it('montar matriz deve distribuir transacoes nos meses', () => {
+    const transacoes : TransacaoImpl[] = [
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        periodicidade: Periodicidade.MENSAL,
+        dataInicial: new Date(2020, 0, 1),
+        dataFinal: new Date(2020, 2, 1),
+        liquidacao: TipoLiquidacao.CONTA
+      },
+      {
+        descricao: 'Transacao 2',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        periodicidade: Periodicidade.MENSAL,
+        dataInicial: new Date(2020, 2, 1),
+        dataFinal: new Date(2020, 2, 1),
+        dataLiquidacao: new Date(2020, 2, 1),
+        liquidacao: TipoLiquidacao.CONTA
+      },
+      {
+        descricao: 'Transacao 3',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        periodicidade: Periodicidade.MENSAL,
+        dataInicial: new Date(2020, 4, 1),
+        dataFinal: new Date(2020, 10, 1),
+        liquidacao: TipoLiquidacao.CONTA
+      }
+    ].map(i=>new TransacaoImpl(i));
+
+    const matriz = service.montarMatriz(transacoes);
+
+    const t1 = matriz.get('Transacao 1');
+    const t2 = matriz.get('Transacao 2');
+    const t3 = matriz.get('Transacao 3');
+    expect(t1).toBeTruthy();
+    expect(t2).toBeTruthy();
+    expect(t3).toBeTruthy();
+    if (!!t1 && !!t2 && !!t3) {
+      expect(t1[0]).toBeTruthy();
+      expect(t1[0].length).toBe(1);
+      expect(t1[1].length).toBe(1);
+      expect(t1[2].length).toBe(1);
+      expect(t1[3].length).toBe(0);
+      expect(t2[0]).toBeTruthy();
+      expect(t2[0].length).toBe(0);
+      expect(t2[1].length).toBe(0);
+      expect(t2[2].length).toBe(1);
+      expect(t2[3].length).toBe(0);
+      for (var mes = 0; mes < 4; mes++) {
+        expect(t3[mes].length).toBe(0);
+      }
+      expect(t3[11].length).toBe(0);
+      for (var mes = 4; mes < 11; mes++) {
+        expect(t3[mes].length).toBe(1);
+      }
+    }
+  })
+
+  it('deve calcular total mensal considerando débitos e créditos', () => {
+    const transacoes : TransacaoImpl[] = [
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.TRANSFERENCIA,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.CREDITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+    ].map(i=>new TransacaoImpl(i));
+
+    const matriz = service.montarMatriz(transacoes);
+
+    const totalAno = service.totalMes(matriz);
+    const totalMes = service.totalMes(matriz, 1);
+
+    expect(totalAno).toBe(-100);
+    expect(totalMes).toBe(0);
+  });
+
+  it('deve calcular total por transacao considerando débitos e créditos', () => {
+    const transacoes : TransacaoImpl[] = [
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.DEBITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 1',
+        valor: 100,
+        tipoTransacao: TipoTransacao.TRANSFERENCIA,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+      {
+        descricao: 'Transacao 2',
+        valor: 100,
+        tipoTransacao: TipoTransacao.CREDITO,
+        dataInicio: new Date(2020,0,1),
+        dataFinal: new Date(2020,0,1)
+      },
+    ].map(i=>new TransacaoImpl(i));
+
+    const matriz = service.montarMatriz(transacoes);
+
+    debugger;
+    const totalAno = service.totalAnual(matriz);
+    const totalT1 = service.totalAnual(matriz, 'Transacao 1');
+    const totalT2 = service.totalAnual(matriz, 'Transacao 2');
+
+    expect(totalAno).toBe(-100);
+    expect(totalT1).toBe(-200);
+    expect(totalT2).toBe(100);
+  });
+
 });
--- app-rebalanceamento/src/app/transacao/services/transacao-matriz.service.ts
+++ app-rebalanceamento/src/app/transacao/services/transacao-matriz.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
 import { TransacaoService } from './transacao.service';
-import { TransacaoImpl } from '../models/transacao.model';
+import { TipoTransacao, TransacaoImpl } from '../models/transacao.model';
 import { map } from 'rxjs';
-import { getMonth, isAfter, isBefore, set, startOfDay } from 'date-fns';
+import { getMonth, isAfter, isBefore, set, setMonth, startOfDay } from 'date-fns';
 
 export type MatrizTransacoes = Map<string, MatrizLinha>;
 
@@ -26,60 +26,116 @@ export class TransacaoMatrizService {
   }
   montarMatriz(transacoes: TransacaoImpl[]): MatrizTransacoes {
 
-    const matriz : MatrizTransacoes = new Map<string, { [mes: number]: TransacaoImpl[]}>();
+    const matriz: MatrizTransacoes = new Map<string, { [mes: number]: TransacaoImpl[] }>();
 
-    transacoes.forEach(transacao=>{
+    this.preencherMatrizComTransacoesInformadas(transacoes, matriz);
+
+    this.preencherMatrizComProjecaoOcorrencias(matriz);
+
+    return matriz;
+  }
+  private preencherMatrizComProjecaoOcorrencias(matriz: MatrizTransacoes) {
+    new Array(...matriz.values()).forEach((matrizLinha: MatrizLinha) => {
+      for (var mes = 1; mes < 12; mes++) {
+        const transacoesMes = matrizLinha[mes];
+        if (!transacoesMes.length && matrizLinha[mes - 1].length) {
+          const novasTransacoes = this.copiarTransacoesParaMes(matrizLinha, mes);
+
+          if (novasTransacoes.length) {
+            this.preencherMatrizComTransacoesInformadas(novasTransacoes, matriz);
+          }
+        }
+      }
+    });
+  }
+
+  private copiarTransacoesParaMes(matrizLinha: MatrizLinha, mes: number) {
+    return matrizLinha[mes - 1]
+      .filter(item => !item.dataFinal || !isAfter(setMonth(item.dataInicial, mes), item.dataFinal))
+      .map(original => {
+        const novaTransacao = new TransacaoImpl(original);
+        delete novaTransacao._id;
+        delete novaTransacao.dataLiquidacao;
+        novaTransacao.dataInicial = setMonth(original.dataInicial, mes);
+        novaTransacao.origem = original._id || original.origem;
+        return novaTransacao;
+      });
+  }
+
+  private preencherMatrizComTransacoesInformadas(transacoes: TransacaoImpl[], matriz: MatrizTransacoes) {
+    transacoes.forEach(transacao => {
       if (!matriz.has(transacao.descricao)) {
-        const item : MatrizLinha = {};
-        for (let i = 0; i < 12; i++) {item[i]=[];}
+        const item: MatrizLinha = {};
+        for (let i = 0; i < 12; i++) { item[i] = []; }
         matriz.set(transacao.descricao, item);
       }
 
       const linha = matriz.get(transacao.descricao);
       if (!!linha) {
-        const mes = getMonth(transacao.dataInicial) - 1;
-        linha[mes].push(transacao);        
+        const mes = getMonth(transacao.dataInicial);
+        linha[mes].push(transacao);
       }
     });
-
-
-    Object.values(matriz).forEach((matrizLinha: MatrizLinha)=>{
-      console.log(matrizLinha)
-      Object.keys(matrizLinha).forEach(mes => {
-        console.log(mes);
-      });
-    })
-/*
-    matriz.forEach(linhaTransacoes=>{
-
-      Object.keys(linhaTransacoes).forEach(transacaoDescricao => { 
-        const linhaTransacoes = linhaTransacoes.get(transacaoDescricao);
-      });
-      linhaTransacoes.forEach((transacoesMes, mes) => {
-        if (mes < 11) {
-          const dataMes = startOfDay(set(new Date(), {month: mes +1, date: 1})) 
-          transacoesMes.forEach(transacao=>{
-            if (! transacao.dataFinal || isBefore(transacao.dataFinal, dataMes)) {
-              const novaTransacao = new TransacaoImpl(transacao);
-              delete novaTransacao._id;
-              delete novaTransacao.dataLiquidacao;
-              novaTransacao.origem = transacao._id || transacao.origem;
-
-              const encontrou = linhaTransacoes[mes+1].findIndex(transacao=>transacao.descricao === novaTransacao.descricao);
-              if (encontrou < 0) {
-                linhaTransacoes[mes+1].push(novaTransacao);
-              }
-            }
-          });
-        }
-      })
-    })
-*/
-
-    return matriz;
   }
-  totalMes(matriz: MatrizTransacoes, mes: number) {
 
+  totalMes(matriz: MatrizTransacoes, mes?: number) {
+    const linhasMatriz = new Array(...matriz.keys());
+
+    return linhasMatriz.reduce((acc: number, descricao: string) => {
+      const linhaTransacao = matriz.get(descricao);
+      if (!linhaTransacao) return 0;
+
+      let transacoes: TransacaoImpl[] = obterTransacoes(linhaTransacao);
+
+      return acc + transacoes
+        .filter(transacao => [TipoTransacao.DEBITO, TipoTransacao.CREDITO].includes(transacao.tipoTransacao))
+        .reduce((tacc, transacao) => {
+          const fator = transacao.tipoTransacao === TipoTransacao.DEBITO ? -1 : 1;
+          return tacc + fator * transacao.valor;
+        }, 0);
+        
+      }, 0);
+      
+      function obterTransacoes(linhaTransacao: MatrizLinha) {
+        let transacoes: TransacaoImpl[];
+      if (!!mes) {
+        transacoes = linhaTransacao[mes];
+      }
+      else {
+        transacoes = Object.values(linhaTransacao)
+          .reduce((tacc, transacao) => {
+            return tacc.concat(transacao);
+          }, []);
+        }
+      return transacoes;
+    }
+  }
+
+  totalAnual(matriz: MatrizTransacoes, descricao?: string): number {
+    const linhasMatriz = new Array(...matriz.keys()).filter(item=> !descricao || descricao === item);
+    
+    return linhasMatriz.reduce((acc: number, chaveMatriz: string)=>{
+      const linhaTransacao = matriz.get(chaveMatriz);
+      
+      if (!linhaTransacao) return 0;
+      
+      const matrizTransacao = Object.values(linhaTransacao);
+      
+      const somaMatrizTransacao = matrizTransacao.reduce((lacc, transacoesMes) => {
+
+        const somaTransacoesMes = transacoesMes
+          .filter(transacao => [TipoTransacao.DEBITO, TipoTransacao.CREDITO].includes(transacao.tipoTransacao))
+          .reduce((tacc, transacao) => {
+            const fator = transacao.tipoTransacao === TipoTransacao.DEBITO ? -1 : 1;
+            return tacc + fator * transacao.valor;
+          }, 0);
+
+        return lacc + somaTransacoesMes;
+      }, 0);
+
+      return acc + somaMatrizTransacao
+
+    }, 0);
   }
 
 }
