44c8c1effd2e3193da92ea14d78738a6d27b39fe
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-05-28 16:05:05 -0300

    Remove módulos despesa e recebimento

--- app-rebalanceamento/src/app/recebimento/models/recebimento.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RecebimentoImpl } from './recebimento.model';
-
-describe('Recebimento', () => {
-  it('should create an instance', () => {
-    expect(new RecebimentoImpl({})).toBeTruthy();
-  });
-});
