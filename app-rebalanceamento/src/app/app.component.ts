4675734a28602e439ffa4ff623b711cf788249b1
Marcelo Damasceno <mdamasceno@globality.com.br>
2024-06-18 12:29:31 -0300

    feat: obtém as cotações das moedas cadastradas.

--- app-rebalanceamento/src/app/app.component.ts
+++ app-rebalanceamento/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CacheService } from './util/services/cache.service';
+import { forkJoin, map } from 'rxjs';
+import { CotacaoService } from './cotacao/services/cotacao.service';
+import { Moeda } from './ativos/model/ativos.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app-rebalanceamento';
+
+  cotacoes: any;
+
+  constructor(
+    private _cacheService: CacheService,
+    private _cotacaoService: CotacaoService
+  ) {}
+
+  ngOnInit(): void {
+      this.obterCotacoesMoedas();
+  }
+
+  
+  obterCotacoesMoedas() {
+    const cotacoes = this._cacheService.get('cotacoesMoedas');
+    if (!cotacoes) {
+      forkJoin({
+        USDBRL: this._cotacaoService.obterCotacaoMoeda(Moeda.USD, Moeda.BRL),
+        USDTUSD: this._cotacaoService.obterCotacaoMoeda(Moeda.USDT, Moeda.USD),
+      }).pipe(
+        map(resultado => {
+          const mapCotacoes = new Map<string, number>();
+          
+          const usdBRL = resultado.USDBRL?.preco || 0;
+          const usdtUSD = resultado.USDTUSD?.preco || 0;
+          const brlUSD = resultado.USDBRL ? 1 / resultado.USDBRL?.preco : 0;
+          const usdUSDT = resultado.USDTUSD ? 1 / resultado.USDTUSD?.preco : 0;
+          const usdtBRL = (resultado.USDTUSD?.preco || 0) * (resultado.USDBRL?.preco || 0);
+
+          mapCotacoes.set("USDBRL", usdBRL);
+          mapCotacoes.set("USDTUSD", usdtUSD);
+
+          mapCotacoes.set("BRLUSD", brlUSD);
+          mapCotacoes.set("USDUSDT", usdUSDT);
+
+          mapCotacoes.set("USDTBRL", usdtBRL );
+          mapCotacoes.set("USDUSDT", 1 / usdtBRL);
+
+          this._cacheService.set('cotacoesMoedas', mapCotacoes);
+          return mapCotacoes;
+        })
+      ).subscribe(cotacoes => this.cotacoes = cotacoes);
+    };
+  }
+
 }
--- app-rebalanceamento/src/app/ativos/carteira-portifolio/carteira-portifolio.component.ts
+++ app-rebalanceamento/src/app/ativos/carteira-portifolio/carteira-portifolio.component.ts
@@ -1,14 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { CarteiraFormComponent } from '../carteira-form/carteira-form.component';
-import { CarteiraImpl, Moeda } from '../model/ativos.model';
-import { CarteiraService } from '../services/carteira.service';
 import { AlertService } from 'src/app/services/alert.service';
-import { Observable, forkJoin, map, of } from 'rxjs';
-import { CacheService } from 'src/app/util/services/cache.service';
-import { CotacaoImpl } from 'src/app/cotacao/models/cotacao.model';
-import { CotacaoService } from 'src/app/cotacao/services/cotacao.service';
+import { CarteiraImpl } from '../model/ativos.model';
+import { CarteiraService } from '../services/carteira.service';
 
 // type CombinacoesMoedas = "BRLUSD" | "USDBRL" | "BRLUSDT" | "USDTBRL" | "USDTUSD" | "USDUSDT";
 
@@ -29,16 +23,12 @@ export class CarteiraPortifolioComponent implements OnInit{
 
   constructor(
     private _carteiraService: CarteiraService,
-    private _cotacaoService: CotacaoService,
-    private _cacheService: CacheService,
     private _alertService: AlertService,
     private _route: ActivatedRoute,
-    private _modal: NgbModal
   ) {}
 
   ngOnInit(): void {
     this.idCarteira = this._route.snapshot.params['carteira'];
-    this.obterCotacoesMoedas();
     this.obterCarteiras();
   }
 
@@ -49,38 +39,6 @@ export class CarteiraPortifolioComponent implements OnInit{
     else {
       this._carteiraService.obterCarteira(this.idCarteira).subscribe(carteira=>carteira && (this.carteiras = [carteira]));
     }
-  }
-
-  obterCotacoesMoedas() {
-    const cotacoes = this._cacheService.get('cotacoesMoedas');
-    if (!cotacoes) {
-      forkJoin({
-        USDBRL: this._cotacaoService.obterCotacaoMoeda(Moeda.USD, Moeda.BRL),
-        USDTUSD: this._cotacaoService.obterCotacaoMoeda(Moeda.USDT, Moeda.USD),
-      }).pipe(
-        map(resultado => {
-          const mapCotacoes = new Map<string, number>();
-          
-          const usdBRL = resultado.USDBRL?.preco || 0;
-          const usdtUSD = resultado.USDTUSD?.preco || 0;
-          const brlUSD = resultado.USDBRL ? 1 / resultado.USDBRL?.preco : 0;
-          const usdUSDT = resultado.USDTUSD ? 1 / resultado.USDTUSD?.preco : 0;
-          const usdtBRL = (resultado.USDTUSD?.preco || 0) * (resultado.USDBRL?.preco || 0);
-
-          mapCotacoes.set("USDBRL", usdBRL);
-          mapCotacoes.set("USDTUSD", usdtUSD);
-
-          mapCotacoes.set("BRLUSD", brlUSD);
-          mapCotacoes.set("USDUSDT", usdUSDT);
-
-          mapCotacoes.set("USDTBRL", usdtBRL );
-          mapCotacoes.set("USDUSDT", 1 / usdtBRL);
-
-          this._cacheService.set('cotacoesMoedas', mapCotacoes);
-          return mapCotacoes;
-        })
-      ).subscribe();
-    };
   }
 
   adicionarCarteira() {
